ba00fee69903d8cba75b06a5d2b4a59596bd9549
nao_e <nao_e@nao-laptop.localdomain>
2023-06-28 15:05:52 +0900

    modify directory

---
+++ terraform/book/security_group.tf
@@ -0,0 +1,22 @@
+resource "aws_security_group" "ingress-all" {
+  name   = "ingress_all"
+  vpc_id = aws_vpc.book_vpc.id
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+  }
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
--- terraform/book/vpc.tf
+++ terraform/book/vpc.tf
@@ -1,5 +1,6 @@
 resource "aws_vpc" "book_vpc" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
   tags = {
     Name = "book_vpc"
   }
