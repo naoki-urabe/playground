46b9b5d2440f49281ee2004c755b3df9cc1b635f
naoki-urabe <na0k1ura6e@gmail.com>
2024-04-28 22:48:47 +0900

    nat設定追加

--- terraform/book/vpc.tf
+++ terraform/book/vpc.tf
@@ -29,18 +29,47 @@ resource "aws_internet_gateway" "book_igw" {
   }
 }
 
-resource "aws_route_table" "book_rt" {
+resource "aws_route_table" "book_rtb" {
   vpc_id = aws_vpc.book_vpc.id
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.book_igw.id
   }
   tags = {
-    Name = "book_rt"
+    Name = "book_rtb"
   }
 }
 
 resource "aws_route_table_association" "book_public_association" {
   subnet_id      = aws_subnet.book_public.id
-  route_table_id = aws_route_table.book_rt.id
+  route_table_id = aws_route_table.book_rtb.id
+}
+
+resource "aws_nat_gateway" "nat" {
+  subnet_id     = aws_subnet.book_public.id
+  allocation_id = aws_eip.nat.id
+  tags = {
+    Name = "NAT"
+  }
+  depends_on = [aws_internet_gateway.book_igw]
+}
+
+resource "aws_eip" "nat" {
+
+}
+
+resource "aws_route_table" "nat_rtb" {
+  vpc_id = aws_vpc.book_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.nat.id
+  }
+  tags = {
+    Name = "nat_rtb"
+  }
+}
+
+resource "aws_route_table_association" "nat_association" {
+  subnet_id      = aws_subnet.book_private.id
+  route_table_id = aws_route_table.nat_rtb.id
 }
