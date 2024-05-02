42980f644a857cae060a9fd7e86dfed0facde6c0
naoki-urabe <na0k1ura6e@gmail.com>
2024-05-02 21:55:54 +0900

    php-mysql追加

--- terraform/book/init.sh
+++ terraform/book/init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt update
-sudo apt install -y apache2 mariadb-server php php-mbstring
+sudo apt install -y apache2 mariadb-server php php-mbstring php-mysql
 sudo service apache2 start
 cd /home/ubuntu
 wget https://nodejs.org/dist/v18.16.1/node-v18.16.1-linux-x64.tar.xz
